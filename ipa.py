f7a3247b70ce852e11acef3a36b5b2f0da91e8a5
Daniel Franco <dfranco123@hotmail.com>
2020-12-17 19:06:18 -0500

    Ingreso logica del proceso

--- ipa.py
+++ ipa.py
@@ -2,31 +2,49 @@ import requests
 from bs4 import BeautifulSoup
 
 
-word = str(input("Ingresa una palabra: ")).lower()
-URL = ("https://en.wiktionary.org/wiki/" + word)
-response = requests.get(URL)
+def wiktionary():
+    word = str(input("Ingresa una palabra: ")).lower()
+    URL = ("https://en.wiktionary.org/wiki/" + word)
+    response = requests.get(URL)
 
-soup = BeautifulSoup(response.text, "html.parser")
-titles = soup.select("span.ib-content.qualifier-content ")
+    soup = BeautifulSoup(response.text, "html.parser")
+    titles = soup.select("span.ib-content.qualifier-content")
 
-if titles:
-  type_ipa = []
+    if titles:
+        type_ipa = []
 
-  for title in titles:
-    if title.a:
-      if word in title.text:
-        continue
-      else: 
-        type_ipa.append(title.text)
+        for title in titles:
+            if title.a:
+                if word in title.text:
+                    continue
+                else:
+                    type_ipa.append(title.text)
 
-  ipas_list = soup.select("span.IPA")
+        ipas_list = soup.select("span.IPA")
 
-  ipas =[ipa.text for ipa in ipas_list]
-  dic = dict(zip(type_ipa, ipas))
-
-  print(dic)
-
-else:
-  print("Word not find")
+        ipas = [ipa.text for ipa in ipas_list]
+        dic = dict(zip(type_ipa, ipas))
+        print(dic)
+    else:
+        print("Word not find")
 
 
+
+def lexico():
+    pass
+
+
+if __name__ == "__main__":
+    pagina = int(input("""
+   Wiktionary [1]
+   Lexico [2]
+   """))
+
+    if pagina == 1:
+        print("Wiktionary")
+        wiktionary()
+    elif pagina == 2:
+        print("Lexico")
+        lexico()
+    else:
+        print("Ingresa una opción correcta")
