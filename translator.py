fcec3bf5dbe64f70c9c258137d987e82d07dbc4d
Daniel Franco <dfranco123@hotmail.com>
2020-12-20 22:54:50 -0500

    Traigo la ultima version

--- translator.py
+++ translator.py
@@ -1,9 +1,6 @@
 import main
-<<<<<<< HEAD
-=======
 import detectlanguage
 from googletrans import Translator
->>>>>>> nicoll
 from deep_translator import LingueeTranslator
 from deep_translator import GoogleTranslator
 
@@ -20,16 +17,6 @@ def solicitud():
     word = str(input("Ingresa una palabra: ")).lower()
     if not word:
         solicitud()
-<<<<<<< HEAD
-    return word
-
-def linguee_en_es():
-    word = solicitud()
-    
-    translated = LingueeTranslator(source='en', target='es').translate(word, return_all=True)
-    print("")
-
-=======
 
     translator = Translator()
     language   = translator.detect(word)
@@ -52,41 +39,13 @@ def do_translat(translator):
 
 def linguee():
     translated = do_translat(LingueeTranslator)
->>>>>>> nicoll
     for i in translated:
         print(i.capitalize())
     
     run()
 
-<<<<<<< HEAD
-    print("")
-def linguee_es_en():
-    word = solicitud()
-    
-    translated = LingueeTranslator(source='es', target='en').translate(word, return_all=True)
-
-    for i in translated:
-        print(i.capitalize())
-    
-    run()
-
-def googletrans_en_es():
-    word = solicitud()
-    
-    translated = GoogleTranslator(source='en', target='es').translate(text=word)
-        
-    print(translated.capitalize())
-    run()
-
-def googletrans_es_en():
-    word = solicitud()
-    
-    translated = GoogleTranslator(source='es', target='en').translate(text=word)
-        
-=======
 def googletrans():
     translated = do_translat(GoogleTranslator)
->>>>>>> nicoll
     print(translated.capitalize())
     run()
 
