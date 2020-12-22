e125bcac96a8e09bd171a60462c1626917f35af6
Daniel Franco <dfranco123@hotmail.com>
2020-12-21 20:50:35 -0500

    Se agrega soporte para listas y strings en traductor

--- translator.py
+++ translator.py
@@ -10,41 +10,56 @@ choise = """
     
 Ingresa un número: """
 
+
 def solicitud():
     word = str(input("Ingresa una palabra: ")).lower()
     if not word:
         solicitud()
 
     translator = Translator()
-    language   = translator.detect(word)
-    language   = language.lang
+    language = translator.detect(word)
+    language = language.lang
 
-    return word, language
+    if type(language) == list:
+        if language[0] == "en" or language[0] == "es":
+            return word, language[0]
+        else:
+            solicitud()
+    else:
+        if language == "en" or language == "es":
+            return word, language
+        else:
+            solicitud()
+
 
 def do_translat(translator):
-    word,language = solicitud()    
-    
+    word, language = solicitud()
+
     if language == "en":
         dest = "es"
     else:
         dest = "en"
     if translator == LingueeTranslator:
-        translated = translator(source=language, target= dest).translate(word, return_all=True)
+        translated = translator(source=language, target=dest).translate(
+            word, return_all=True)
     else:
-        translated = translator(source=language, target= dest).translate(word)
+        translated = translator(source=language, target=dest).translate(word)
     return translated
+
 
 def linguee():
     translated = do_translat(LingueeTranslator)
     for i in translated:
         print(i.capitalize())
-    
+
     run()
+
 
 def googletrans():
     translated = do_translat(GoogleTranslator)
     print(translated.capitalize())
     run()
+
 
 def run():
     pagina = str(input(choise))
@@ -60,7 +75,7 @@ def run():
     elif pagina == "9":
         print("Atras")
         main.inicio()
-        
+
     else:
         print("Ingresa una opción correcta")
         run()
